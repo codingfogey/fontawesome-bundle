d69657e0dbe90d259fba7beb5565172896f81fa6
Andreas Ernst <codingfogey@gmail.com>
2013-11-25 08:04:42 +0100

    Added more unit tests for the twig extension

--- Tests/Twig/FontAwesomeExtensionTest.php
+++ Tests/Twig/FontAwesomeExtensionTest.php
@@ -33,17 +33,143 @@ class FontAwesomeExtensionTest extends PHPUnit_Framework_TestCase
         $this->assertCount(2, $this->extension->getFunctions());
     }
 
-    public function testfaIconFilter()
+    public function testFaIconFilterSimple()
     {
         $this->assertEquals(
                 '<i class="fa fa-twitter"></i>', $this->extension->faIconFunction('twitter')
         );
     }
 
-    public function testfaStackedIconFilter()
+    public function testFaIconFilterScaleAllowed()
+    {
+        $this->assertEquals(
+                '<i class="fa-3x fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('scale' => '3x'))
+        );
+    }
+
+    public function testFaIconFilterScaleNotAllowed()
+    {
+        $this->assertEquals(
+                '<i class="fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('scale' => '7'))
+        );
+    }
+
+    public function testFaIconFilterFixedWidth()
+    {
+        $this->assertEquals(
+                '<i class="fa-fixed-width fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('fixed-width' => true))
+        );
+    }
+
+    public function testFaIconFilterFixedWidthNonBoolean()
+    {
+        $this->assertEquals(
+                '<i class="fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('fiexed-width' => 'alfred'))
+        );
+    }
+
+    public function testFaIconFilterListIcon()
+    {
+        $this->assertEquals(
+                '<i class="fa-li fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('list-icon' => true))
+        );
+    }
+
+    public function testFaIconFilterListIconNonBoolean()
+    {
+        $this->assertEquals(
+                '<i class="fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('list-icon' => 'alfred'))
+        );
+    }
+
+    public function testFaIconFilterBorder()
+    {
+        $this->assertEquals(
+                '<i class="fa-border fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('border' => true))
+        );
+    }
+
+    public function testFaIconFilterBorderNonBoolean()
+    {
+        $this->assertEquals(
+                '<i class="fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('border' => 'alfred'))
+        );
+    }
+
+    public function testFaIconFilterPull()
+    {
+        $this->assertEquals(
+                '<i class="fa-pull-left fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('pull' => 'left'))
+        );
+    }
+
+    public function testFaIconFilterPullNotAllowed()
+    {
+        $this->assertEquals(
+                '<i class="fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('pull' => 'bit'))
+        );
+    }
+
+    public function testFaIconFilterSpin()
+    {
+        $this->assertEquals(
+                '<i class="fa-spin fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('spin' => true))
+        );
+    }
+
+    public function testFaIconFilterSpinNonBoolean()
+    {
+        $this->assertEquals(
+                '<i class="fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('spin' => 'dr'))
+        );
+    }
+
+    public function testFaIconFilterRotate()
+    {
+        $this->assertEquals(
+                '<i class="fa-rotate-180 fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('rotate' => '180'))
+        );
+    }
+
+    public function testFaIconFilterRotateNotAllowed()
+    {
+        $this->assertEquals(
+                '<i class="fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('rotate' => 'ebgngr'))
+        );
+    }
+
+    public function testFaStackedIconFilter()
     {
         $this->assertEquals(
                 '<span class="fa-stack"><i class="fa-stack-2x fa fa-square-o"></i><i class="fa-stack-1x fa fa-twitter"></i></span>', $this->extension->faStackedIconFunction('twitter', 'square-o')
+        );
+    }
+
+    public function testFaIconFilterFlip()
+    {
+        $this->assertEquals(
+                '<i class="fa-flip-horizontal fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('flip' => 'horizontal'))
+        );
+    }
+
+    public function testFaIconFilterFlipNotAllowed()
+    {
+        $this->assertEquals(
+                '<i class="fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('flip' => 'pilf'))
+        );
+    }
+
+    public function testFaIconFilterInverse()
+    {
+        $this->assertEquals(
+                '<i class="fa-inverse fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('inverse' => true))
+        );
+    }
+
+    public function testFaIconFilterInverseNonBoolean()
+    {
+        $this->assertEquals(
+                '<i class="fa fa-twitter"></i>', $this->extension->faIconFunction('twitter', array('inverse' => 1234))
         );
     }
 
